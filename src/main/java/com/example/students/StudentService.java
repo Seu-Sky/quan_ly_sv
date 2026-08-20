86fc8e1ab64ff67a26a497902c540444638d7f74
Seu-Sky <yseu17924@gmail.com>
2026-08-20 08:09:29 +0700

    đẩy lần thứ nhất

---
+++ src/main/java/com/example/students/StudentService.java
@@ -0,0 +1,16 @@
+package com.example.students;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class StudentService {
+    
+    @Autowired
+    private StudentRepository repository;
+
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+}
